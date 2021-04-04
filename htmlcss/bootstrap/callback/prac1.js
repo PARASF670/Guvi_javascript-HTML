eda0bd8bc01d2c4784afb354a49b81bf34eb3fe2
parasf <parasf@gmail.com>
2021-04-04 22:30:20 +0530

    countdown task

--- htmlcss/bootstrap/callback/prac1.js
+++ htmlcss/bootstrap/callback/prac1.js
@@ -1,27 +1,45 @@
-var x = 10;
+
 setTimeout(()=>{
-    // console.log(x);
-    document.querySelector("#board").innerHTML = x;
+    
+    document.querySelector("#board").innerHTML = 10;
     setTimeout(()=>{
-        //console.log(x-1);
-        document.querySelector("#board").innerHTML = x-1;
+        
+        document.querySelector("#board").innerHTML = 9;
         setTimeout(() => {
-            //console.log(x - 2);
-            document.querySelector("#board").innerHTML = x-2;
+            
+            document.querySelector("#board").innerHTML = 8;
         
             setTimeout(() => {
-               // console.log(x - 3);
-               document.querySelector("#board").innerHTML = x-3;
+               
+                document.querySelector("#board").innerHTML = 7;
                 setTimeout(() => {
-                    //console.log(x - 4);
-                    document.querySelector("#board").innerHTML = x-4;
+                    
+                    document.querySelector("#board").innerHTML = 6;
                     setTimeout(() => {
-                        document.querySelector("#board").innerHTML = "Happy Independence Day";
+                        document.querySelector("#board").innerHTML = 5;
+                        setTimeout(() => {
+            
+                            document.querySelector("#board").innerHTML = 4;
+                        
+                            setTimeout(() => {
+                               
+                                document.querySelector("#board").innerHTML = 3;
+                                setTimeout(() => {
+                                    
+                                    document.querySelector("#board").innerHTML = 2;
+                                    setTimeout(() => {
+                                    
+                                        document.querySelector("#board").innerHTML = 1;
+                                        setTimeout(() => {
+                                            document.querySelector("#board").innerHTML = "Happy Independence Day";
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
                     }, 1000)
-                },1000)
-                
+                }, 1000)
             }, 1000)
-        }, 1000)
+        },1000)
     },1000)
-},1000
-)
+},1000)
